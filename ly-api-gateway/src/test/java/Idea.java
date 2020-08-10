93cf28c2f10b71410b8bbb8b80464808861b24dc
bill <316120837@qq.com>
2020-08-10 18:04:34 +0800

    try to add test class

---
+++ ly-api-gateway/src/test/java/Idea.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 清角吹寒
+ * Date: 2020-03-21
+ * Time: 21:05
+ */
+public class Idea {
+    public static void main(String[] args) {
+        if (true){
+
+        }else {
+
+        }
+    }
+}
